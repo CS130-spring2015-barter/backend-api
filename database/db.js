47f304a3e3cd6d923c7631474617484df7cad082
MatteoVH <matteovh@ucla.edu>
2015-04-21 21:02:19 -0700

    Database will now be returned in a callback function for ease of use as a module.

--- database/db.js
+++ database/db.js
@@ -3,20 +3,22 @@ var conString = "postgres://barter:swipeyswipe@db.raycoll.me/barter";
 
 var db = {};
 
-pg.connect(conString, function(err, client, done) {
-	if (err)
-		return console.error('error fetching client from pool', err);
+module.exports = function(callback) {
+	pg.connect(conString, function(err, client, done) {
+		if (err)
+			return console.error('error fetching client from pool', err);
 
-	//example query 
-	/*client.query('SELECT $1::int AS number', ['1'], function(err, result) {
-		// call `done()` to release the client back to the pool 
-		done();
-		if(err) {
-			return console.error('error running query', err);
-		}
-		console.log(result.rows[0].number);
-		client.end();
-	});*/
-});
+		//example query 
+		/*client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+			// call `done()` to release the client back to the pool 
+			done();
+			if(err) {
+				return console.error('error running query', err);
+			}
+			console.log(result.rows[0].number);
+			client.end();
+		});*/
 
-module.exports = db;
+		callback(err, db);
+	});
+};

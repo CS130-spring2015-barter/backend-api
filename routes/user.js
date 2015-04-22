8223ad1a56d086bcf1308f2dd95e3409bb014bcb
MatteoVH <matteovh@ucla.edu>
2015-04-21 22:35:49 -0700

    Cobbled together database connection with routes.

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function(db) {
+	var express = require('express');
+	var router = express.Router();
 
-//Register a new user. Values passed will be email address and hashed password.
-//All values will be in req.body
-router.post('/user', function(req, res, next) {
-	
-	res.send('index');
-});
+	//Register a new user. Values passed will be email address and hashed password.
+	//All values will be in req.body
+	router.get('/', function(req, res, next) {
+		res.send('userget');
+	});
 
-module.exports = router;
+	return router;
+};

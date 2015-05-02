83b08e5ea5ea6e52c025ee90038ca04fecc7fd20
MatteoVH <matteovh@ucla.edu>
2015-05-01 21:48:42 -0700

    Updated the description for the user GET route.

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,7 @@ module.exports = function(db) {
 	var express = require('express');
 	var router = express.Router();
 
-	//Register a new user. Values passed will be email address and hashed password.
-	//All values will be in req.body
+	//login a user
 	router.get('/', function(req, res, next) {
 		res.send('userget');
 	});

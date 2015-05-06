449e19e654392eb4431b49ba64492a94b53e4f08
MatteoVH <matteovh@ucla.edu>
2015-05-05 19:15:42 -0700

    Added route for adding items.

--- routes/item.js
+++ routes/item.js
@@ -1,14 +1,46 @@
 module.exports = function(db) {
 	var express = require('express');
 	var router = express.Router();
+	var uuid = require('node-uuid');
 
-	router.get('/:userId', function(req, res, next) {
-		db.createItem(req.body, function(err, itemCreated) {
+	//set an item as liked by a certain user
+	router.post('/liked', function(req, res, next) {
+		var data = {};
+		data.uid = req.body.userId;
+		data.likesRegistered = true;
+		for (var x = 0; x < req.body.itemIds; x++) {
+			data.iid = req.body.itemIds[x];
+			db.addItemLiked(data, function(err, itemLikeRegistered) {
+				if (err) next(err);
+				if (!itemLikeRegistered)
+					data.likesRegistered = false;
+			});
+		}
+
+		if (!data.likesRegistered)
+			res.sendStatus(500);
+		else
+			res.sendStatus(200);
+	});
+
+	//create a new item
+	router.post('/', function(req, res, next) {
+		var image = req.body.item_picture;
+		
+		var imageName = uuid.v1();
+		fs.writeFile(imageName, image, function(err) {
 			if (err) next(err);
-			if (itemCreated)
-				res.sendStatus(200);
-			else
-				res.sendStatus(500);
+
+			//replace the image with the image name which is what we're actually storing in the db.
+			req.body.item_picture = imageName;
+
+			db.createItem(req.body, function(err, itemCreated) {
+				if (err) next(err);
+				if (itemCreated)
+					res.sendStatus(200);
+				else
+					res.sendStatus(500);
+			});
 		});
 	});
 

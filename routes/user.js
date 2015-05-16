8600fc22d2a64a60d456b2e6c664b41898255613
Steven Collison <stevendonv@gmail.com>
2015-05-16 12:58:58 -0700

    Stores passwords with bcrypt during user creation

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ module.exports = function(db) {
 	var passport = require('passport');
 	var expjwt = require('express-jwt');
 	var jwt = require('jwt-simple');
+	var bcrypt = require('bcrypt');
 	var router = express.Router();
 
 	//gets user info for a specific :userId
@@ -37,14 +38,35 @@ router.post('/login', function(req, res, next) {
 	});
 
 
-	//Register a new user. Values passed will be email address and password.
-	//All values will be in req.body
+	//Register a new user.
 	router.post('/', function(req, res, next) {
-		db.createUser(req.body, function(err, userRegistered) {
-			if (err) {
-				return next(err);
+		if (!req.body.password) {
+			return res.status(400).send({message: "Must provide password!"});
+		}
+		if (!req.body.email) {
+			return res.status(400).send({message: "Must provide email!"});
+		}
+		if (!req.body.first || !req.body.last) {
+			return res.status(400).send({message: "Must provide first/last name!"});
+		}
+
+		// Make sure user doesnt already exist
+		db.getPass(req.body.email, function(err, result) {
+			if (result.rows.length != 0) {
+				return res.status(400).send({message: "User already exists!"});
 			}
-			res.send(200);
+
+			// Create bcrypted password and save user
+			var salt = bcrypt.genSaltSync(10);
+			var passwordHash = bcrypt.hashSync(req.body.password, salt);
+			req.body.password = passwordHash;
+
+			db.createUser(req.body, function(err, userRegistered) {
+				if (err) {
+					return next(err);
+				}
+				res.send(200);
+			});
 		});
 	});
 

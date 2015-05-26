73e0bafa87a8b010126b5590e25d7e2d762f7da5
MatteoVH <matteovh@ucla.edu>
2015-05-26 16:37:41 -0700

    Increased size of requests allowed to 100MB

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ module.exports = function(dbObject) {
 	// uncomment after placing your favicon in /public
 	//app.use(favicon(__dirname + '/public/favicon.ico'));
 	app.use(logger('dev'));
-	app.use(bodyParser.json());
+	app.use(bodyParser.json({limit: 100000000}));
 	app.use(bodyParser.urlencoded({ extended: false }));
 	app.use(cookieParser());
 	app.use(express.static(path.join(__dirname, 'public')));
